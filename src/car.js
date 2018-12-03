04134a818259237d79b959fcf1555d146af56d1a
nlg550 <38725499+nlg550@users.noreply.github.com>
2018-12-03 19:17:44 +0000

    Car Movement

---
+++ src/car.js
@@ -0,0 +1,102 @@
+import loader from "./loader.js";
+
+function createCar(scene) {
+
+    //Right SpotLight
+    let spotLightR = new THREE.SpotLight(0xffffff, 0.8)
+    spotLightR.decay = 2
+    spotLightR.penumbra = 0.45
+    //spotLightR.castShadow = true
+    spotLightR.position.set(60, 50, 200)
+    spotLightR.target = new THREE.Object3D()
+    spotLightR.target.position.set(80, 50, 50000)
+    spotLightR.target.updateMatrixWorld()
+    spotLightR.add(spotLightR.target)
+
+    //Left SpotLight
+    let spotLightL = new THREE.SpotLight(0xffffff, 0.8)
+    spotLightL.decay = 2
+    spotLightL.penumbra = 0.45
+    //spotLightL.castShadow = true
+    spotLightL.position.set(-60, 50, 200)
+    spotLightL.target = new THREE.Object3D()
+    spotLightL.target.position.set(-80, 50, 50000)
+    spotLightL.target.updateMatrixWorld()
+    spotLightL.add(spotLightL.target)
+
+    let car = loader.loadObject("../models/Lamborginhi/Body.mtl", "../models/Lamborginhi/Body.obj")
+    let wheels = []
+    wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
+    wheels[0].name = "WheelRF"
+    wheels[0].position.set(-85.429, 34.599, 128.173)
+    wheels[0].rotation.y = Math.PI
+    car.add(wheels[0])
+    wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
+    wheels[1].name = "WheelLF"
+    wheels[1].position.set(85.429, 34.599, 128.173)
+    car.add(wheels[1])
+    wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
+    wheels[2].name = "WheelRB"
+    wheels[2].position.set(-85.429, 36.416, -146.734)
+    wheels[2].rotation.y = Math.PI
+    car.add(wheels[2])
+    wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
+    wheels[3].name = "WheelLB"
+    wheels[3].position.set(85.429, 36.416, -146.734)
+    car.add(wheels[3])
+
+    car.add(spotLightR)
+    car.add(spotLightL)
+
+    car.scale.set(0.001, 0.001, 0.001)
+
+    scene.add(car)
+
+    return {
+        body: car,
+        wheels: wheels,
+        spotLightL: spotLightL,
+        spotLightR: spotLightR,
+        speed: 0,
+        acceleration: 1,
+        life: 5,
+        turn_speed: 1
+    }
+}
+
+function move(car, amount, clock) {
+
+    if (car.speed * amount > 0) car.speed += amount * car.acceleration * clock.getDelta()
+    else if (amount) car.speed += amount * (car.acceleration * 4) * clock.getDelta()
+    else {
+        if (car.speed > 0.01) car.speed -= car.acceleration * 3 * clock.getDelta()
+        else if (car.speed < -0.01) car.speed += car.acceleration * 3 * clock.getDelta()
+        else car.speed = 0
+    }
+
+    if (car.speed > 0.5) car.speed = 0.5
+    else if (car.speed < -0.5) car.speed = -0.5
+
+    car.body.translateZ(car.speed)
+    car.body.updateMatrixWorld()
+}
+
+function rotate(car, amount, clock) {
+
+    const rot = car.turn_speed * amount * clock.getDelta()
+    car.body.rotateY(rot)
+    car.body.updateMatrixWorld()
+}
+
+function reset(car) {
+    car.life = 5
+    car.body.position.set(0, 0, 0)
+    car.body.rotation.set(0, 0, 0)
+}
+
+export default {
+    createCar,
+    move,
+    rotate,
+    reset
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import loader from "./loader.js";
+import carMethods from "./car.js";
 
 //Scene
 let scene = new THREE.Scene()
 
+//Clock
+let clock = new THREE.Clock()
+
 //Camera
-let followCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
-followCamera.position.set(50, 50, 50)
-const orthoCamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 1000)
-orthoCamera.position.set(0, 130, 0)
-const topCamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 1000)
-topCamera.position.set(0, 800, 0)
+let followCamera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100000)
+followCamera.position.set(30, 30, 30)
+const orthoCamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 100000)
+orthoCamera.position.set(0, 50, 0)
+const topCamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 100000)
+topCamera.position.set(0, 100, 0)
+let currentCamera = followCamera
+
+followCamera.lookAt(scene.position)
+//scene.add(followCamera)
 
 //Renderer
-let renderer = new THREE.WebGLRenderer()
+let renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    logarithmicDepthBuffer: true
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.gammaOutput = true
 document.body.appendChild(renderer.domElement)
@@ -24,7 +35,7 @@ controls.campingFactor = 0.25
 controls.enableZoom = true
 
 //Light
-let dirLight = new THREE.DirectionalLight(0xffffff, 0.0)
+let dirLight = new THREE.DirectionalLight(0xffffff, 0.7)
 dirLight.position.set(150, 150, -150)
 // dirLight.castShadow = true
 // dirLight.shadow = new THREE.LightShadow(followCamera)
@@ -33,70 +44,77 @@ dirLight.position.set(150, 150, -150)
 // dirLight.shadow.mapSize.width = 1024
 scene.add(dirLight)
 
-let ambLight = new THREE.AmbientLight(0xffffff, 0.0)
+let ambLight = new THREE.AmbientLight(0xffffff, 0.4)
 ambLight.position.set(200, 200, 200)
 scene.add(ambLight)
 
-let spotLightR = new THREE.SpotLight(0xffffff, 0.8)
-spotLightR.decay = 2
-spotLightR.penumbra = 0.45
-//spotLightR.castShadow = true
-spotLightR.position.set(60, 50, 200)
-spotLightR.target = new THREE.Object3D()
-spotLightR.target.position.set(80, 50, 50000)
-spotLightR.target.updateMatrixWorld()
-spotLightR.add(spotLightR.target)
-
-let spotLightL = new THREE.SpotLight(0xffffff, 0.8)
-spotLightL.decay = 2
-spotLightL.penumbra = 0.45
-//spotLightL.castShadow = true
-spotLightL.position.set(-60, 50, 200)
-spotLightL.target = new THREE.Object3D()
-spotLightL.target.position.set(-80, 50, 50000)
-spotLightL.target.updateMatrixWorld()
-spotLightL.add(spotLightL.target)
-
-console.log(spotLightL.target.position.x + " " + spotLightL.target.position.y + " " + spotLightL.target.position.z)
-
-
 //Objects
-let car = loader.loadObject("../models/Lamborginhi/Body.mtl", "../models/Lamborginhi/Body.obj")
-let wheels = []
-wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
-wheels[0].name = "WheelRF"
-wheels[0].position.set(-85.429, 34.599, 128.173)
-wheels[0].rotation.y = Math.PI
-car.add(wheels[0])
-wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
-wheels[1].name = "WheelLF"
-wheels[1].position.set(85.429, 34.599, 128.173)
-car.add(wheels[1])
-wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
-wheels[2].name = "WheelRB"
-wheels[2].position.set(-85.429, 36.416, -146.734)
-wheels[2].rotation.y = Math.PI
-car.add(wheels[2])
-wheels.push(loader.loadObject("../models/Lamborginhi/Wheel.mtl", "../models/Lamborginhi/Wheel.obj"))
-wheels[3].name = "WheelLB"
-wheels[3].position.set(85.429, 36.416, -146.734)
-car.add(wheels[3])
-
-car.add(spotLightR)
-car.add(spotLightL)
-
-car.scale.set(0.01, 0.01, 0.01)
-scene.add(car)
-
+let car = carMethods.createCar(scene)
+car.body.add(followCamera)
 const table = loader.loadObject("../models/table/table.mtl", "../models/table/table.obj")
 scene.add(table)
+
+scene.add(new THREE.GridHelper(1000, 10))
+
+//Keyboard Input
+document.body.addEventListener("keydown", event => {
+    switch (event.key) {
+        case "1":
+            currentCamera = followCamera
+            break
+        case "2":
+            currentCamera = orthoCamera
+            break
+        case "3":
+            currentCamera = topCamera
+            break
+        case "w":
+            carMethods.move(car, 1, clock)
+            break
+        case "s":
+            carMethods.move(car, -1, clock)
+            break
+        case "a":
+            carMethods.rotate(car, 1, clock)
+            break
+        case "d":
+            carMethods.rotate(car, -1, clock)
+            break
+        case "z":
+            carMethods.reset(car)
+            break
+        default:
+            break;
+    }
+}, false)
+
+document.body.addEventListener("keyup", event => {
+    switch (event.key) {
+        case "w":
+            carMethods.move(car, 0, clock)
+            break
+        case "s":
+            carMethods.move(car, 0, clock)
+            break
+        default:
+            break;
+    }
+}, false)
+
+loop()
 
 
 //Game Loop
 function loop() {
+
     requestAnimationFrame(loop)
-    controls.update()
-    renderer.render(scene, followCamera)
+    onUpdate()
+    renderer.render(scene, currentCamera)
 }
 
-loop()
+//Update
+function onUpdate() {
+
+    controls.update()
+    followCamera.lookAt(car.body.position)
+}
